835b3227dbfe886ea33737be2faecc08ccf8e6a3
Daniel <danielseguraf@gmail.com>
2019-09-05 23:58:52 -0500

    More data-test Attribute Tests

--- src/App.test.js
+++ src/App.test.js
@@ -5,16 +5,29 @@ import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
-test('renders without error', () => {
+const setup = (props={}, state=null) => {
+  return shallow(<App {...props} />)
+}
 
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`)
+}
+
+test('renders without error', () => {
+  const appComponent = findByTestAttr(setup(), 'component-app')
+  expect(appComponent.length).toBe(1)
 })
 
 test('renders increment button', () => {
-
+  const wrapper = setup()
+  const button = findByTestAttr(wrapper, 'increment-button')
+  expect(button.length).toBe(1)
 })
 
 test('renders counter display', () => {
-
+  const wrapper = setup()
+  const counter = findByTestAttr(wrapper, 'counter-display')
+  expect(counter.length).toBe(1)
 })
 
 test('counter starts at 0', () => {
